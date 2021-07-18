a9653b6e959aee94a8c0a21a1b48e3ae354064b0
Vali <valibojici@gmail.com>
2021-07-18 12:04:43 +0300

    update

--- assets/peripheral-vision/per_vision.js
+++ assets/peripheral-vision/per_vision.js
@@ -2,6 +2,7 @@
 let startButton = document.getElementById('start-button');
 let startContainer = document.getElementById('start-container');
 let resultsContainer = document.getElementById('results-container');
+let tryagainButton = document.getElementById('try-again-button');
 let mainContainer = document.getElementById('main-container');
 let circle = document.getElementById('circle');
 let carContainer = document.getElementById('car-container');
@@ -26,11 +27,24 @@ let total_corect_answers = 0;
 let best_reaction_time = 100000;
 
 startButton.addEventListener('click', e=>{
+    setup_and_start(); 
+});
 
+tryagainButton.addEventListener('click', e=>{
+    setup_and_start();
+})
+
+window.onresize = e=>{
+    circle.style.width = window.getComputedStyle(circle).height;
+    radius = parseFloat(circle.style.width) / 2;
+};
+
+function setup_and_start()
+{
     startContainer.style.display = 'none';
     resultsContainer.style.display = 'none';
     timer = 60;
-    timer_container.textContent = `Time left 00:${timer}`;
+    timer_container.textContent = `Time left ${display_time(timer)}`;
     timer_container.style.display = 'inline';
 
     correct_answers = 0;
@@ -41,78 +55,62 @@ startButton.addEventListener('click', e=>{
 
     setTimeout(()=>{
         countdown_container.textContent = '3';
+    }, 0);
+
+    setTimeout(()=>{
+        countdown_container.textContent = '2';
+    }, 1000);
+
+    setTimeout(()=>{
+        countdown_container.textContent = '1';
+
         setTimeout(()=>{
-            countdown_container.textContent = '2';
-            setTimeout(()=>{
-                countdown_container.textContent = '1';
-
-                timer_interval = setInterval(()=>{
-                    timer--;
-                    timer_container.textContent = `Time left 00:${timer}`;
+            countdown_container.textContent = '';
             
-                    if(timer == 0){
-                        clearInterval(timer_interval);
-                    }
-                }, 1000);
-            
-                setTimeout(()=>{
-                    run();
+            timer_interval = setInterval(()=>{
+                timer--;
+                timer_container.textContent = `Time left ${display_time(timer)}`;
+        
+                if(timer == 0){
+                    clearInterval(timer_interval);
+                }
+            }, 1000);
 
-                    countdown_container.textContent = '';
-                }, 1000)
-            
-                
-                
-            }, 1000)
-
+            run();
         }, 1000)
-
-    }, 0)
-
     
-    
-    
-});
+    }, 2000)
 
-window.onresize = e=>{
-    circle.style.width = window.getComputedStyle(circle).height;
-    radius = parseFloat(circle.style.width) / 2;
-};
+}
+
  
 function run(){
     if (timer == 0)
     {
-        console.log('Done');
-        mainContainer.style.display = 'none';
-        resultsContainer.style.display = 'flex';
-        timer_container.style.display = 'none';
-
-        correct_answers_span.textContent = total_corect_answers;
-        incorrect_answers_span.textContent = total_incorrect_answers;
-        accuracy_span.textContent = Math.round((total_corect_answers / (total_corect_answers + total_incorrect_answers)) * 100) + '%';
-        best_time_span.textContent = best_reaction_time + ' ms';
+        terminate();
+        
         return;
     }
 
     mainContainer.style.display = 'block';
     circle.style.width = window.getComputedStyle(circle).height;
     let radius = parseFloat(circle.style.width) / 2;
+
     let reaction_time = window.localStorage.getItem('reaction_time') != null 
-                            ? parseInt(window.localStorage.reaction_time) : 500;
-    
+                            ? parseInt(window.localStorage.reaction_time) : 500;    
 
     console.log(reaction_time, incorrect_answers, correct_answers);
 
-    let wrong_answer = false; // true is incorrect car or sign is clicked
+    let wrong_answer = false; // true if incorrect car or sign is clicked
 
     function draw_static_tiles()
     {
         for(let i = 15 + 7.5; i < 360 + 15 + 7.5  ; i += 45)
         {
             let img = document.createElement('img');
-            img.src = './assets/imgs/static.png';
-            img.classList.add('tile');
-            img.classList.add('static');
+            img.src = './imgs/static.png';
+            img.classList.add('tile', 'static', 'noselect');
+
             let radius_reduced = 0.9 * radius;
             let left = radius + radius_reduced * Math.cos(degrees_to_radians(i)) + 'px';
             let top = radius + radius_reduced * Math.sin(degrees_to_radians(-i)) + 'px';
@@ -129,9 +127,11 @@ function run(){
         {
             let childs = circle.children;
             let index = random_int(0, 7) * 3 + 1;
-            childs.item(index).src = './assets/imgs/route66logo.png';
-            childs.item(index).style.width = '85px';
-            childs.item(index).style.height = '85px';
+            childs.item(index).src = './imgs/route66logo.png';
+
+            let curr_size = parseInt(window.getComputedStyle(childs.item(index)).width);
+            childs.item(index).style.width = `${curr_size * 1.25}px`;
+            childs.item(index).style.height = `${curr_size * 1.25}px`;
             return (index - 1) / 3;
         }
 
@@ -140,8 +140,8 @@ function run(){
             for(let i = start; i < 360 + start; i += increment)
             {
                 let img = document.createElement('img');
-                img.src = './assets/imgs/horse.png';
-                img.classList.add('tile');
+                img.src = './imgs/horse.png';
+                img.classList.add('tile', 'noselect');
                 let radius_reduced = radius_offset * radius;
                 let left = radius + radius_reduced * Math.cos(degrees_to_radians(i)) + 'px';
                 let top = radius + radius_reduced * Math.sin(degrees_to_radians(-i)) + 'px';
@@ -164,28 +164,32 @@ function run(){
     function addCar(index)
     {
         let car = document.createElement('img');
-        car.src = `./assets/imgs/car${index}.png`;
-        carContainer.appendChild(car);
+        car.src = `./imgs/car${index}.png`;
         car.style.display = 'inline';
+        car.classList.add('noselect');
+        carContainer.appendChild(car);
         return car;
     }
 
 
-    let route66_index = draw_tiles();
-    let car_index = random_int(1,3);
-    addCar(car_index);
-    
+    // draw all tiles and route66 logo and the car
+    let route66_index, car_index;
+    setTimeout(()=>{
+        route66_index = draw_tiles();
+        car_index = random_int(1,3);
+        addCar(car_index);
+    }, 0)
 
+    // after reaction time milliseconds remove everything and draw static
     setTimeout(() => {
         remove_children(circle);
         draw_static_tiles();
         remove_children(carContainer);
-        carContainer.style.backgroundImage = 'url("./assets/imgs/static.png")';
+        carContainer.style.backgroundImage = 'url("./imgs/static.png")';
     }, reaction_time);
 
 
-    let car_answer = null;
-
+    // after 2*reaction time milliseconds, draw 2 cars and static tiles to  choose from
     setTimeout( ()=>{
         remove_children(circle);
         carContainer.style.backgroundImage = 'none';
@@ -197,27 +201,34 @@ function run(){
             if(fake_car_index > 3)fake_car_index = 1;
         }
 
-        let real_car = addCar(car_index);
-        let fake_car = addCar(fake_car_index);
-        real_car.isCorrect = 'true';
-        fake_car.isCorrect = 'false';
 
-        
-
+        let real_car, fake_car;
         if(random_int(0, 1) == 0)
         {
-            // 50% chance cars are swapped
-            [real_car.src, fake_car.src] = [fake_car.src, real_car.src];
-            [real_car, fake_car] = [fake_car, real_car];
+            real_car = addCar(car_index);
+            fake_car = addCar(fake_car_index);
         }
+        else
+        {
+            fake_car = addCar(fake_car_index);
+            real_car = addCar(car_index);
+        }
+        real_car.isCorrect = 'true';
+        fake_car.isCorrect = 'false';
 
         real_car.addEventListener('click', handle_car_click);
         fake_car.addEventListener('click', handle_car_click);
 
         function handle_car_click(e)
         {
-            [fake_car, real_car].forEach(car => car.removeEventListener('click', handle_car_click));
+            if(e.target.isCorrect == 'false'){
+                wrong_answer = true;
+            }
+
+            fake_car.removeEventListener('click', handle_car_click);
+            real_car.removeEventListener('click', handle_car_click);
             remove_children(carContainer);
+
             draw_static_tiles();
 
             let index = 0;
@@ -227,16 +238,11 @@ function run(){
                 tile.addEventListener('click', handle_tile_click);
                 index++;
             }
-
-            if(e.target.isCorrect === 'false')
-            {
-                wrong_answer = true;
-            }
         }
 
         function handle_tile_click(e)
         {
-            if(e.target.isCorrect === 'false'){
+            if(e.target.isCorrect == 'false'){
                 wrong_answer = true;
             }
 
@@ -244,17 +250,15 @@ function run(){
                 tile.removeEventListener('click', handle_tile_click);
             }
 
+            remove_children(circle);
+
             if(wrong_answer == true){
                 incorrect_answers++;
                 total_incorrect_answers++;
-
             }
             else{
                 correct_answers++;
                 total_corect_answers++;
-                if(reaction_time < best_reaction_time){
-                    best_reaction_time = reaction_time;
-                }
             }
 
             if(incorrect_answers == 2){
@@ -278,6 +282,11 @@ function run(){
             }
             
             if(correct_answers == 3){
+                
+                if(reaction_time < best_reaction_time){
+                    best_reaction_time = reaction_time;
+                }
+
                 if(reaction_time >= 800){
                     reaction_time = 500;
                 }
@@ -302,9 +311,6 @@ function run(){
                 correct_answers = 0;
             }
 
-            remove_children(circle);
-            remove_children(carContainer);
-
             setTimeout(()=>{
                 run();
             }, 1000)
@@ -315,11 +321,32 @@ function run(){
     
 }
 
+function terminate()
+{
+    console.log('Done');
+    mainContainer.style.display = 'none';
+    resultsContainer.style.display = 'flex';
+    timer_container.style.display = 'none';
+
+    correct_answers_span.textContent = total_corect_answers;
+    incorrect_answers_span.textContent = total_incorrect_answers;
+    accuracy_span.textContent = Math.round((total_corect_answers / (total_corect_answers + total_incorrect_answers)) * 100) + '%';
+    best_time_span.textContent = best_reaction_time + ' ms';
+}
+
 function degrees_to_radians(degrees)
 {
     return degrees * (Math.PI/180);
 }
  
+function display_time(timer)
+{
+    // returns string in format mm:ss
+    let minutes = Math.floor(timer / 60);
+    let seconds = timer % 60;
+    return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
 function random_int(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
