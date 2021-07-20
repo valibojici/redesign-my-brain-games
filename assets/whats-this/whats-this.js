bc50502fbca94b750e17ffe65d5d0442346ca2b6
Vali <valibojici@gmail.com>
2021-07-20 18:02:19 +0300

    update

--- assets/whats-this/whats-this.js
+++ assets/whats-this/whats-this.js
@@ -14,17 +14,42 @@ window.addEventListener('keydown',e=>{
     console.log(e.key);
     if(e.key == 'ArrowLeft' || e.key == 'ArrowRight'){
         let arrow = e.key == 'ArrowLeft' ? leftArrow : rightArrow;
-        changeArrowColor(arrow, 10, 200, 10);
-        setTimeout(()=>{
-            changeArrowColor(arrow, 0, 0, 0);
-        }, 75)
+        flashGreen(arrow);
     }
 })
 
-function changeArrowColor(arrow, r, g, b, alpha = 1){
-    arrow.children[0].style.fill = `rgba(${r}, ${g}, ${b}, ${alpha})`;
+function flashRed(arrow){
+    arrow.classList.toggle('red-arrow-color');
+    setTimeout(()=>{
+        arrow.classList.toggle('red-arrow-color');
+    }, 120);
 }
 
-changeArrowColor(leftArrow, 10, 200, 244, 1);
+function flashGreen(arrow){
+    arrow.classList.toggle('green-arrow-color');
+    setTimeout(()=>{
+        arrow.classList.toggle('green-arrow-color');
+    }, 120);
+}
 
-console.log(leftArrow.children);
+last5 = [];
+
+
+setInterval(()=>{
+    let img = document.getElementById('game-item');
+    let id = random_int(1, 18);
+    while(last5.length >= 5 && last5.indexOf(id) > -1){
+        id = random_int(1, 18);
+        if(id > 18) id = 1;
+    }
+    if(last5.length >= 5){
+        last5.shift();
+    }
+    last5.push(id);
+    img.src = `./imgs/tools/${id}.png`;
+}, 1000)
+
+
+function random_int(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
