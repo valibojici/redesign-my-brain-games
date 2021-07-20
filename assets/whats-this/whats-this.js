930bcc6fdfb69b8f436eb5051b0db05c23ba05df
Vali <valibojici@gmail.com>
2021-07-20 21:13:58 +0300

    update

--- assets/whats-this/whats-this.js
+++ assets/whats-this/whats-this.js
@@ -1,8 +1,12 @@
-let startContainer = document.getElementById('start-container');
-let gameContainer = document.getElementById('game-container');
-let resultsContainer = document.getElementById('results-container');
+const startContainer = document.getElementById('start-container');
+const gameContainer = document.getElementById('game-container');
+const resultsContainer = document.getElementById('results-container');
+const correctSquaresContainer = document.getElementById('correct-squares');
+const incorrectSquaresContainer = document.getElementById('incorrect-squares');
 
 startContainer.style.display = 'none';
+gameContainer.style.display = 'none';
+resultsContainer.style.display = 'block';
 
 let leftArrow = document.getElementById('left-arrow');
 let rightArrow = document.getElementById('right-arrow');
@@ -14,15 +18,31 @@ window.addEventListener('keydown',e=>{
     console.log(e.key);
     if(e.key == 'ArrowLeft' || e.key == 'ArrowRight'){
         let arrow = e.key == 'ArrowLeft' ? leftArrow : rightArrow;
-        flashGreen(arrow);
+        flashRed(arrow);
     }
 })
+
+for(let i=0;i<5;++i){
+    let square = document.createElement('div');
+    square.classList.add('square', 'green-square');
+    correctSquaresContainer.appendChild(square);
+
+    if(i % 2 == 0)square.classList.add('active-square')
+}
+
+for(let i=0;i<2;++i){
+    let square = document.createElement('div');
+    square.classList.add('square', 'red-square');
+    incorrectSquaresContainer.appendChild(square);
+
+    if(i==1)square.classList.add('active-square')
+}
 
 function flashRed(arrow){
     arrow.classList.toggle('red-arrow-color');
     setTimeout(()=>{
         arrow.classList.toggle('red-arrow-color');
-    }, 120);
+    }, 130);
 }
 
 function flashGreen(arrow){
@@ -31,24 +51,6 @@ function flashGreen(arrow){
         arrow.classList.toggle('green-arrow-color');
     }, 120);
 }
-
-last5 = [];
-
-
-setInterval(()=>{
-    let img = document.getElementById('game-item');
-    let id = random_int(1, 18);
-    while(last5.length >= 5 && last5.indexOf(id) > -1){
-        id = random_int(1, 18);
-        if(id > 18) id = 1;
-    }
-    if(last5.length >= 5){
-        last5.shift();
-    }
-    last5.push(id);
-    img.src = `./imgs/tools/${id}.png`;
-}, 1000)
-
 
 function random_int(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
