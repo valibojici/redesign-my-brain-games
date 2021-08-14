0bf2eec3b0945a1f1d30695da95a9010edad34db
Vali <valibojici@gmail.com>
2021-08-14 20:54:48 +0300

    update

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,21 @@
-button = document.getElementsByTagName('button')[0];
-prev = performance.now();
-button.addEventListener('click', e=>{
-    t = performance.now();
-    console.log(t - prev);
-    prev = t;
-});
+// let mainContainer = document.getElementById('main-container');
+// let media = window.matchMedia("(max-width:1000px)");
+
+// if(media.matches){
+//     calculate();
+// }
+
+// media.addEventListener('change', e=>{
+    
+//         mainContainer.style.gridTemplateColumns = '1fr 1fr';
+//         calculate();
+    
+    
+    
+// });
+
+
+// function calculate(){
+//     mainContainer.style.width = parseInt(getComputedStyle(mainContainer).height) + 'px';
+//     console.log(mainContainer.style.width);
+// }
