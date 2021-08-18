19260b56af7c5c18549762d5bb34dcf64ba03fe8
Vali <valibojici@gmail.com>
2021-08-18 13:24:09 +0300

    update

--- assets/peripheral-vision/peripheral-vision.js
+++ assets/peripheral-vision/peripheral-vision.js
@@ -26,16 +26,17 @@ function setup() {
     const resultsContainer = document.getElementById('results-container');
     const timerContainer = document.getElementById('timer-container');
     const countdownContainer = document.getElementById('countdown');
-    
+
     const testGenerator = generateTest();
-    
+    let answers = [];
+
     startContainer.classList.add('hide');
     gameContainer.classList.remove('hide');
     countdownContainer.classList.remove('hide');
 
     let secondsLeft = document.getElementById('seconds-input').value;
     let secondsInterval = null;
-    let reactionTime = document.getElementById('reaction-time-input').value;
+    let reactionTime = parseInt(document.getElementById('reaction-time-input').value);
 
     timerContainer.textContent = `Time left ${calculateTime(secondsLeft)}`;
 
@@ -75,277 +76,187 @@ function setup() {
 
     function run() {
         if (secondsLeft === 0) {
-            terminate();
+            terminate(answers);
             return;
         }
 
         let test = testGenerator.next().value;
 
-        function stageOne(){
+        stageOne();
+
+        function stageOne() {
             let container = document.getElementById('stage-one');
-            let tile = container.getElementsByClassName('outer-tiles')[0].children[test.tile];
+            let tile = container.getElementsByClassName('outer-tiles')[0].children[test.tile * 3 + 1];
             let car = container.getElementsByClassName('car-container')[0].getElementsByTagName('img')[0];
-            console.log(tile, car);
-            
+
             // make sure all images are loaded first
-            let images = [
-                {elem:tile , src:'./imgs/logo.png'}, 
-                {elem:car, src:`./imgs/car${test.car}.png`}
-            ].map(item=>{
-                return new Promise((resolve, reject)=>{
+            let images = [{
+                    elem: tile,
+                    src: './imgs/logo.png'
+                },
+                {
+                    elem: car,
+                    src: `./imgs/car${test.car}.png`
+                }
+            ].map(item => {
+                return new Promise((resolve, reject) => {
                     item.elem.src = item.src;
-                    item.elem.onload = resolve;
-                    item.elem.onerror = reject;
+                    if (item.elem.complete) {
+                        resolve();
+                    } else {
+                        item.elem.onload = resolve;
+                        item.elem.onerror = reject;
+                    }
                 });
             });
-            
-            Promise.all(images).then((vals)=>{
-                console.log(vals, 'images loaded');
+
+            Promise.all(images).then((vals) => {
                 container.classList.remove('hide');
+                setTimeout(()=>{
+                    tile.src = './imgs/horse.png';
+                    stageTwo();
+                }, reactionTime);
             })
         }
 
-        stageOne();
+        function stageTwo() {
+            document.getElementById('stage-one').classList.add('hide');
+            document.getElementById('stage-two').classList.remove('hide');
+            setTimeout(stageThree, reactionTime);
+        }
+
+        function stageThree() {
+            document.getElementById('stage-two').classList.add('hide');
+            let container = document.getElementById('stage-three');
+            let tilesContainer = container.getElementsByClassName('tiles')[0];
+            let carContainer = container.getElementsByClassName('car-container')[0];
+            let carImgs = carContainer.getElementsByTagName('img');
+
+            let answerIsCorrect = true;
+
+            tilesContainer.classList.add('hide');
+            carContainer.classList.add('hide');
+            container.classList.remove('hide');
+
+            let carIndexes = (randomInt(0, 1) == 0) ? [test.car, test.fakeCar] : [test.fakeCar, test.car];
+
+            let images = [{
+                    image: carImgs[0],
+                    index: carIndexes[0]
+                },
+                {
+                    image: carImgs[1],
+                    index: carIndexes[1]
+                }
+            ].map(item => {
+                return new Promise((resolve, reject) => {
+                    item.image.src = `./imgs/car${item.index}.png`;
+                    item.image.isCorrect = (item.index == test.car);
+                    if (item.image.complete) {
+                        resolve();
+                    } else {
+                        item.image.onload = resolve;
+                        item.image.onerror = reject;
+                    }
+                });
+            });
+
+            Promise.all(images).then(() => {
+                carContainer.classList.remove('hide');
+                for (let car of carImgs) {
+                    car.addEventListener('click', handleCarClick);
+                    car.classList.add('selectable');
+                }
+            });
+
+
+            function handleCarClick(event) {
+                for (let car of carImgs) {
+                    car.removeEventListener('click', handleCarClick);
+                    car.classList.remove('selectable');
+                }
+                answerIsCorrect = event.target.isCorrect;
+
+                carContainer.classList.add('hide');
+                tilesContainer.classList.remove('hide');
+
+                for (let i = 0; i < tilesContainer.children.length; ++i) {
+                    tilesContainer.children[i].isCorrect = (i == test.tile);
+                    tilesContainer.children[i].classList.add('selectable');
+                    tilesContainer.children[i].addEventListener('click', handleTileClick);
+                }
+            }
+
+            function handleTileClick(event) {
+                for (let i = 0; i < tilesContainer.children.length; ++i) {
+                    tilesContainer.children[i].classList.remove('selectable');
+                    tilesContainer.children[i].removeEventListener('click', handleTileClick);
+                }
+
+                answerIsCorrect = (answerIsCorrect && event.target.isCorrect);
+                tilesContainer.classList.add('hide');
+                container.classList.add("hide");
+
+                answers.push({isCorrect : answerIsCorrect, time : reactionTime});
+
+                reactionTime = checkAnswer(answerIsCorrect, reactionTime);
+                setTimeout(run, 1000);
+            }
+        }
+
     }
 }
 
-
-function run() {
-    if (secondsLeft == 0) {
-        terminate();
-        return;
+function checkAnswer(isCorrect, reactionTime){
+    if(checkAnswer.correctAnswers == null || checkAnswer.correctAnswers == undefined){
+        checkAnswer.correctAnswers = 0;
+    }
+    if(checkAnswer.incorrectAnswers == null || checkAnswer.incorrectAnswers == undefined){
+        checkAnswer.incorrectAnswers = 0;
+    }
+    if(isCorrect){
+        checkAnswer.correctAnswers++;
+    } else {
+        checkAnswer.incorrectAnswers++;
     }
 
-    
-
-    document.getElementById('stage-one').classList.remove('hide');
-
-    mainContainer.style.display = 'block';
-    resizeCircleContainer();
-    let radius = parseFloat(circle.style.width) / 2;
-
-    let reaction_time = window.localStorage.getItem('reaction_time') != null ?
-        parseInt(window.localStorage.reaction_time) : 500;
-
-    console.log(reaction_time, incorrect_answers, correct_answers);
-
-    let wrong_answer = false; // true if incorrect car or sign is clicked
-
-    function draw_static_tiles() {
-        for (let i = 15 + 7.5, j = 0; i < 360 + 15 + 7.5; i += 45, j++) {
-            let img = document.createElement('img');
-            img.src = './imgs/static.png';
-            img.classList.add('tile', 'static', 'noselect');
-
-            let radius_reduced = 0.9 * radius;
-            let left = radius + radius_reduced * Math.cos(degrees_to_radians(i)) + 'px';
-            let top = radius + radius_reduced * Math.sin(degrees_to_radians(-i)) + 'px';
-
-            img.style.left = left;
-            img.style.top = top;
-            circle.appendChild(img);
-        }
-    }
-
-    function draw_tiles() {
-        function change_to_logo() {
-            let childs = circle.children;
-            let index = random_int(0, 7) * 3 + 1;
-            childs.item(index).src = './imgs/route66logo.png';
-
-            let curr_size = parseInt(window.getComputedStyle(childs.item(index)).width);
-            childs.item(index).style.width = `${curr_size * 1.25}px`;
-            childs.item(index).style.height = `${curr_size * 1.25}px`;
-            return (index - 1) / 3;
-        }
-
-        function draw(radius_offset, start, increment, isOuter = false) {
-            for (let i = start; i < 360 + start; i += increment) {
-                let img = document.createElement('img');
-                img.src = './imgs/horse.png';
-                img.classList.add('tile', 'noselect');
-                img.style.transform = `translate(-50%, -50%) scaleX(${1 / parseFloat(circle.style.transform.substr(22))})`;
-                let radius_reduced = radius_offset * radius;
-                let left = radius + radius_reduced * Math.cos(degrees_to_radians(i)) + 'px';
-                let top = radius + radius_reduced * Math.sin(degrees_to_radians(-i)) + 'px';
-
-                img.style.left = left;
-                img.style.top = top;
-                circle.appendChild(img);
-            }
-            if (isOuter) {
-                return change_to_logo();
-            }
-        }
-        let route66_index = draw(0.9, 7.5, 15, true);
-        draw(0.7, 0, 22.5);
-        draw(0.5, 22.5, 45);
-        return route66_index;
-    }
-
-    function addCar(index) {
-        let car = document.createElement('img');
-        car.src = `./imgs/car${index}.png`;
-        car.style.display = 'inline';
-        car.classList.add('noselect');
-        carContainer.appendChild(car);
-        return car;
-    }
-
-
-    // draw all tiles and route66 logo and the car
-    let route66_index, car_index;
-    setTimeout(() => {
-        route66_index = draw_tiles();
-        car_index = random_int(1, 3);
-        addCar(car_index);
-    }, 0)
-
-    // after reaction time milliseconds remove everything and draw static
-    setTimeout(() => {
-        remove_children(circle);
-        draw_static_tiles(route66_index);
-        remove_children(carContainer);
-        carContainer.style.backgroundImage = 'url("./imgs/static.png")';
-    }, reaction_time);
-
-
-    // after 2*reaction time milliseconds, draw 2 cars and static tiles to  choose from
-    setTimeout(() => {
-        remove_children(circle);
-        carContainer.style.backgroundImage = 'none';
-
-        let fake_car_index = random_int(1, 3);
-        while (fake_car_index == car_index) {
-            fake_car_index = fake_car_index + 1;
-            if (fake_car_index > 3) fake_car_index = 1;
-        }
-
-        let real_car, fake_car;
-        if (random_int(0, 1) == 0) {
-            real_car = addCar(car_index);
-            fake_car = addCar(fake_car_index);
+    if(checkAnswer.correctAnswers === 4 || checkAnswer.incorrectAnswers === 3){
+        checkAnswer.correctAnswers = 0;
+        checkAnswer.incorrectAnswers = 0;
+        if(isCorrect){
+            if(reactionTime >= 2000) return reactionTime - 300;
+            else if(reactionTime >= 1000) return reactionTime - 200;
+            else if(reactionTime >= 500) return reactionTime - 100;
+            else if(reactionTime - 50 >= 100) return reactionTime - 50;
+            else return 100;
         } else {
-            fake_car = addCar(fake_car_index);
-            real_car = addCar(car_index);
+            if(reactionTime >= 2000) return reactionTime + 200;
+            else if(reactionTime >= 1000) return reactionTime + 150;
+            else if(reactionTime >= 500) return reactionTime + 75;
+            else if(reactionTime >= 200) return reactionTime + 50;
+            else return reactionTime + 30;
         }
-        real_car.isCorrect = true;
-        fake_car.isCorrect = false;
-
-        real_car.addEventListener('click', handle_car_click);
-        fake_car.addEventListener('click', handle_car_click);
-
-        function handle_car_click(e) {
-            if (!e.target.isCorrect) {
-                wrong_answer = true;
-            }
-
-            fake_car.removeEventListener('click', handle_car_click);
-            real_car.removeEventListener('click', handle_car_click);
-            remove_children(carContainer);
-
-            draw_static_tiles();
-
-            let index = 0;
-            for (let tile of circle.children) {
-                tile.isCorrect = (index === route66_index);
-                tile.addEventListener('click', handle_tile_click);
-                index++;
-            }
-        }
-
-        function handle_tile_click(e) {
-            if (!e.target.isCorrect) {
-                wrong_answer = true;
-            }
-
-            for (let tile of circle.children) {
-                tile.removeEventListener('click', handle_tile_click);
-            }
-
-            remove_children(circle);
-
-            if (wrong_answer) {
-                incorrect_answers++;
-                total_incorrect_answers++;
-            } else {
-                correct_answers++;
-                total_corect_answers++;
-
-                if (reaction_time < best_reaction_time) {
-                    best_reaction_time = reaction_time;
-                }
-            }
-
-            if (incorrect_answers == 2) {
-                if (reaction_time >= 800) {
-                    reaction_time += 20;
-                } else if (reaction_time >= 500) {
-                    reaction_time += random_int(5, 10);
-                } else if (reaction_time >= 300) {
-                    reaction_time += random_int(10, 20)
-                } else if (reaction_time >= 250) {
-                    reaction_time += random_int(15, 25)
-                } else if (reaction_time >= 200) {
-                    reaction_time += random_int(30, 60);
-                } else if (reaction_time >= 150) {
-                    reaction_time += random_int(40, 80);
-                } else if (reaction_time >= 100) {
-                    reaction_time += random_int(50, 100);
-                } else if (reaction_time >= 70) {
-                    reaction_time += random_int(50, 120);
-                } else {
-                    reaction_time += random_int(50, 170);
-                }
-                window.localStorage.reaction_time = reaction_time;
-                incorrect_answers = 0;
-                correct_answers = 0;
-            }
-
-            if (correct_answers == 3) {
-                if (reaction_time >= 800) {
-                    reaction_time = 500;
-                } else if (reaction_time >= 500) {
-                    reaction_time -= random_int(100, 180);
-                } else if (reaction_time >= 400) {
-                    reaction_time -= random_int(80, 120);
-                } else if (reaction_time >= 300) {
-                    reaction_time -= random_int(65, 100);
-                } else if (reaction_time >= 250) {
-                    reaction_time -= random_int(35, 50);
-                } else if (reaction_time >= 200) {
-                    reaction_time -= random_int(10, 30);
-                } else if (reaction_time >= 140) {
-                    reaction_time -= random_int(10, 15);
-                } else if (reaction_time >= 70) {
-                    reaction_time -= 8;
-                } else reaction_time -= 5;
-
-                window.localStorage.reaction_time = reaction_time;
-                correct_answers = 0;
-                incorrect_answers = 0;
-            }
-
-            setTimeout(() => {
-                run();
-            }, 1000)
-        }
-
-    }, reaction_time * 2);
-
-
+    }
+    return reactionTime;
 }
 
-function terminate() {
-    console.log('Done');
-    mainContainer.style.display = 'none';
-    resultsContainer.style.display = 'flex';
-    timer_container.style.display = 'none';
+function terminate(answers){
+    console.log(answers);
+    let correctAnswers = answers.filter(answer => answer.isCorrect == true).length;
+    let incorrectAnswers = answers.filter(answer => answer.isCorrect != true).length;
+    
+    let bestReactionTime = (correctAnswers == 0) ? 0 : answers
+        .filter(answer => answer.isCorrect)
+        .reduce((min, curr) => min.time < curr.time ? min : curr).time;
 
-    correct_answers_span.textContent = total_corect_answers;
-    incorrect_answers_span.textContent = total_incorrect_answers;
-    accuracy_span.textContent = Math.round((total_corect_answers / (total_corect_answers + total_incorrect_answers)) * 100) + '%';
-    best_time_span.textContent = best_reaction_time + ' ms';
+
+    document.getElementById('correct').textContent = correctAnswers;
+    document.getElementById('incorrect').textContent = incorrectAnswers;
+    document.getElementById('accuracy').textContent = Math.round(100 * correctAnswers / (correctAnswers + incorrectAnswers));
+    document.getElementById('best-react').textContent = bestReactionTime;
+    
+    document.getElementById('game-container').classList.add('hide');
+    document.getElementById('results-container').classList.remove('hide');
 }
 
 function setupStages() {
@@ -448,7 +359,7 @@ function* generateTest() {
             currentCarIndex = randomInt(1, 3);
         }
 
-        while(fakeCarIndex === currentCarIndex){
+        while (fakeCarIndex === currentCarIndex) {
             fakeCarIndex = randomInt(1, 3);
         }
 
@@ -461,7 +372,7 @@ function* generateTest() {
         yield {
             tile: currentTileIndex,
             car: currentCarIndex,
-            fakeCar : fakeCarIndex
+            fakeCar: fakeCarIndex
         }
     }
 }
